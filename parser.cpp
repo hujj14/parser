51e8a9c0b323fa8b975eaeb59adda0edf8c26482
Mihail <smyshmish@gmail.com>
2024-12-09 22:04:31 +0300

    проверка на ровно и заглавные буквы

--- parser.cpp
+++ parser.cpp
@@ -1,11 +1,42 @@
-﻿// parser.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 using namespace std;
-
-
 int main()
 {
-	cout << "Hello world" << endl;
+	setlocale(LC_ALL, "rus");
+	string name;
+	getline(cin, name);
+	string name_var;
+	for (char a : name)
+	{
+		if (a != '=')
+		{
+			name_var += a;
+		}
+		else
+		{
+			break;
+		}
+	}
+	for (char a : name_var)
+	{
+		if (!(isupper(a)))
+		{
+			cout << "Назвние вашей переменной содержит символы в нижнем регистре" << endl;
+			break;
+		}
+
+	}
+	char  space = ' ';
+	int sch = 0;
+	for (char a : name)
+	{
+		if (a == space)
+		{
+			cout << "В строчке не может быть пробелов" << endl;
+			break;
+		}
+	}
+
+
 }
